73b315cf456dc8a45aeac19f5e6cee9a5ce58e1c
glaspal6 <dylan.kamstra@protonmail.com>
2023-10-26 16:35:54 +0200

    Scanner for reserved words

--- lox/Scanner.py
+++ lox/Scanner.py
@@ -7,16 +7,34 @@ class Scanner:
 		self.__tokens: list[Token] = []
 
 		# Track where the scanner is in the source code
-		self.start: int = 0		# Start of the lexme
-		self.current: int = 0	# Current character
-		self.line: int = 1		# Line number
+		self.__start: int = 0		# Start of the lexme
+		self.__current: int = 0	# Current character
+		self.__line: int = 1		# Line number
+		self.__keywords: dict = {
+			"and" : 	TokenType.AND,
+			"class" : 	TokenType.CLAS,
+			"else" : 	TokenType.ELSE,
+			"false" : 	TokenType.FALSE,
+			"for" : 	TokenType.FOR,
+			"fun" : 	TokenType.FUN,
+			"if" : 		TokenType.IF,
+			"nil" : 	TokenType.NIL,
+			"or" : 		TokenType.OR,
+			"print" : 	TokenType.PRINT,
+			"return" : 	TokenType.RETURN,
+			"super" : 	TokenType.SUPER,
+			"this" : 	TokenType.THIS,
+			"true" : 	TokenType.TRUE,
+			"var" : 	TokenType.VAR,
+			"while" : 	TokenType.WHILE,
+		}
 
 	def scanTokens(self) -> list[Token]:
 		while (not self.__isAtEnd()):
-			start = self.current
+			start = self.__current
 			self.__scanToken()
 
-		self.__tokens.append(Token(TokenType.EOF, "", None, self.line))
+		self.__tokens.append(Token(TokenType.EOF, "", None, self.__line))
 		return self.__tokens
 
 	def __scanToken(self):
@@ -50,14 +68,24 @@ class Scanner:
 			# Useless characters
 			case ' ' | '\\r' | '\\t': None
 			case '\\n':
-				self.line += 1
+				self.__line += 1
 
 			# Literals
 			case '"': self.__string()
 
 			case _:
-				if (self.__isDigit(c)): self.__number()
-				else: Lox.error(self.line, "Unexpected character.")
+				if self.__isDigit(c): self.__number()
+				elif self.__isAlpha(c): self.__identifier()
+				else: Lox.error(self.__line, "Unexpected character.")
+
+	def __identifier(self):
+		while self.__isAlphaNumeric(self.__peek()): self.__advance()
+
+		text: str = self.__source[self.__start, self.__current]
+		tType: TokenType = self.__keywords[text]
+		if not tType: tType = TokenType.IDENTIFIER
+
+		self.__addToken(tType)
 
 	def __number(self):
 		while self.__isDigit(self.__peek()): self.__advance()
@@ -69,54 +97,63 @@ class Scanner:
 
 			while self.__isDigit(self.__peek()): self.__advance()
 
-		self.__addToken(TokenType.NUMBER, float(self.__source[self.start, self.current]))
+		self.__addToken(TokenType.NUMBER, float(self.__source[self.__start, self.__current]))
 
 	def __string(self):
 		while p:=self.__peek() != '"' and not self.__isAtEnd():
-			if p == '\\n': self.line += 1
+			if p == '\\n': self.__line += 1
 			self.__advance()
 
 		if self.__isAtEnd():
-			Lox.error(self.line, "Unterminated string.")
+			Lox.error(self.__line, "Unterminated string.")
 			return
 		
 		# The closing "
 		self.__advance()
 
 		# Trim the quotes and add the token
-		value: str = self.__source[self.start+1, self.current-1]
+		value: str = self.__source[self.__start+1, self.__current-1]
 		self.__addToken(TokenType.STRING, value)
 
 	def __isAtEnd(self) -> bool:
-		return self.current >= len(self.__source)
+		return self.__current >= len(self.__source)
 
 	def __advance(self) -> chr:
-		self.current += 1
-		return self.__source[self.current-1]
+		self.__current += 1
+		return self.__source[self.__current-1]
 
-	def __addToken(self, type: TokenType):
-		self.__addToken(self, type, None)
+	def __addToken(self, tType: TokenType):
+		self.__addToken(self, tType, None)
 
-	def __addToken(self, type: TokenType, literal: object):
-		text: str = self.__source[self.start, self.current]
-		self.__tokens.append(Token(type, text, literal, self.line))
+	def __addToken(self, tType: TokenType, literal: object):
+		text: str = self.__source[self.__start, self.__current]
+		self.__tokens.append(Token(tType, text, literal, self.__line))
 
 	def __match(self, expected: chr) -> bool:
 		if self.__isAtEnd(): return False
-		if self.__source[self.current] != expected: return False
+		if self.__source[self.__current] != expected: return False
 
-		self.current += 1
+		self.__current += 1
 		return True
 
 	def __peek(self) -> chr:
 		if self.__isAtEnd(): return '\0'
-		return self.__source[self.current]
+		return self.__source[self.__current]
 	
 	def __peekNext(self) -> chr:
-		if self.current + 1 >= len(self.__source): return '\\0'
-		return self.__source[self.current + 1]
+		if self.__current + 1 >= len(self.__source): return '\\0'
+		return self.__source[self.__current + 1]
 
-	def __isDigit(c: chr) -> bool:
+	def __isDigit(self, c: chr) -> bool:
 		return c >= '0' and c <= '9'
 
+	def __isAlpha(self, c: chr) -> bool:
+		return (
+			(c >= 'a' and c <= 'z') or
+			(c >= 'A' and c <= 'Z') or 
+			c == '_'
+		)
+	
+	def __isAlphaNumeric(self, c: chr) -> bool:
+		return self.__isAlpha(c) or self.__isDigit(c)
 
