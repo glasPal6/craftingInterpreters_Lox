895e22e33bd867b618d3c10f9150d67a3ea1d111
glaspal6 <dylan.kamstra@protonmail.com>
2023-10-26 11:17:02 +0200

    Added one or more characters to scanner

--- lox/Scanner.py
+++ lox/Scanner.py
@@ -1,6 +1,6 @@
 from TokenType import TokenType
 from Token import Token
-
+from Lox import Lox
 class Scanner:
 	def __init__(self, source: str) -> None:
 		self.__source = source
@@ -12,39 +12,67 @@ class Scanner:
 		self.line: int = 1		# Line number
 
 	def scanTokens(self) -> list[Token]:
-		while (not self.isAtEnd()):
+		while (not self.__isAtEnd()):
 			start = self.current
-			self.scanToken()
+			self.__scanToken()
 
 		self.__tokens.append(Token(TokenType.EOF, "", None, self.line))
 		return self.__tokens
 
-	def scanToken(self):
-		c: chr = self.advance()
-		match c:
-			case '(': self.addToken(TokenType.LEFT_PAREN)
-			case ')': self.addToken(TokenType.RIGHT_PAREN)
-			case '{': self.addToken(TokenType.LEFT_BRACE)
-			case '}': self.addToken(TokenType.RIGHT_BRACE)
-			case ',': self.addToken(TokenType.COMMA)
-			case '.': self.addToken(TokenType.DOT)
-			case '-': self.addToken(TokenType.MINUS)
-			case '+': self.addToken(TokenType.PLUS)
-			case ';': self.addToken(TokenType.SEMICOLON)
-			case '*': self.addToken(TokenType.STAR)
+	def __scanToken(self):
+		c: chr = self.__advance()
 
-	def isAtEnd(self) -> bool:
+		match c:
+			# Single character tokens
+			case '(': self.__addToken(TokenType.LEFT_PAREN)
+			case ')': self.__addToken(TokenType.RIGHT_PAREN)
+			case '{': self.__addToken(TokenType.LEFT_BRACE)
+			case '}': self.__addToken(TokenType.RIGHT_BRACE)
+			case ',': self.__addToken(TokenType.COMMA)
+			case '.': self.__addToken(TokenType.DOT)
+			case '-': self.__addToken(TokenType.MINUS)
+			case '+': self.__addToken(TokenType.PLUS)
+			case ';': self.__addToken(TokenType.SEMICOLON)
+			case '*': self.__addToken(TokenType.STAR)
+
+			# One or more character tokens	
+			case '!': self.__addToken(TokenType.BANG_EQUAL if self.__match('=') else TokenType.BANG)
+			case '=': self.__addToken(TokenType.EQUAL_EQUAL if self.__match('=') else TokenType.EQUAL)
+			case '<': self.__addToken(TokenType.LESS_EQUAL if self.__match('=') else TokenType.LESS)
+			case '>': self.__addToken(TokenType.GREATER_EQUAL if self.__match('=') else TokenType.GREATER)
+
+			# For comments and division
+			case '/':
+				if self.__match('/'):
+					while self.__peek() != '\n' and not self.__isAtEnd(): self.__advance()
+				else: self.__addToken(TokenType.SLASH)
+
+			# Useless characters
+
+			case _:
+				Lox.error(self.line, "Unexpected character.")
+
+	def __isAtEnd(self) -> bool:
 		return self.current >= len(self.__source)
 
-	def advance(self) -> chr:
+	def __advance(self) -> chr:
 		self.current += 1
 		return self.__source[self.current-1]
 
-	def addToken(self, type: TokenType):
-		self.addToken(self, type, None)
+	def __addToken(self, type: TokenType):
+		self.__addToken(self, type, None)
 
-	def addToken(self, type: TokenType, literal: object):
+	def __addToken(self, type: TokenType, literal: object):
 		text: str = self.__source[self.start, self.current]
 		self.__tokens.append(Token(type, text, literal, self.line))
 
+	def __match(self, expected: chr) -> bool:
+		if self.__isAtEnd(): return False
+		if self.__source[self.current] != expected: return False
 
+		self.current += 1
+		return True
+
+	def __peek(self) -> chr:
+		if self.__isAtEnd(): return '\0'
+		return self.__source[self.current]
